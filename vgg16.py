42230e1fc2e2e11185bc0fb17c02976b13d0a85d
Preston Price <pwprice57@gmail.com>
2017-03-09 12:53:36 -0700

    change

--- vgg16.py
+++ vgg16.py
@@ -28,16 +28,16 @@ train_labels_file = '/home/icarus/random_labels3058.hdf5'
 train_max = 500
 valid_max = 600
 
-def load_data(data_file, labels_file, train_start, train_end)
-    X_train = HDF5Matrix(data_file, 'dataset', train_start, train_end)
-    y_train = HDF5Matrix(labels_file, 'labels', train_start, train_end)
-    return X_train, y_train
+# def load_data(data_file, labels_file, train_start, train_end)
+#     X_train = HDF5Matrix(data_file, 'dataset', train_start, train_end)
+#     y_train = HDF5Matrix(labels_file, 'labels', train_start, train_end)
+#     return X_train, y_train
 
 X_train = HDF5Matrix(data_file, 'dataset', 0, train_max)
-# X_valid = HDF5Matrix(data_file, 'dataset', train_max, valid_max)
+X_valid = HDF5Matrix(data_file, 'dataset', train_max, valid_max)
 
 y_train = HDF5Matrix(labels_file, 'labels', 0, train_max)
-# y_valid = HDF5Matrix(labels_file, 'labels', train_max, valid_max)
+y_valid = HDF5Matrix(labels_file, 'labels', train_max, valid_max)
 
 # f = h5py.File(train_data_file, "r")
 # f2 = h5py.File(train_labels_file, "r")
@@ -118,17 +118,17 @@ print(model.summary())
 
 
 # def load_data(data_file, labels_file, train_start, train_end, n_training_examples, n_test_examples)
-current = 0
-batch_size = 50
-for i in xrange(epochs):
-	while (current+batch_size <= train_max):
-		X_train, y_train = load_data(train_data_file, train_labels_file, current, current+batch_size)
-		model.fit(X_train, y_train, validation_data=(X_valid, y_valid), nb_epoch=1, batch_size=batch_size)
+# current = 0
+# batch_size = 50
+# for i in xrange(epochs):
+# 	while (current+batch_size <= train_max):
+# 		X_train, y_train = load_data(train_data_file, train_labels_file, current, current+batch_size)
+# 		model.fit(X_train, y_train, validation_data=(X_valid, y_valid), nb_epoch=1, batch_size=batch_size)
 
 
 
 # Fit the model
-# model.fit(X_train, y_train, validation_data=(X_valid, y_valid), nb_epoch=epochs, batch_size=50)
+model.fit(X_train, y_train, validation_data=(X_valid, y_valid), nb_epoch=epochs, batch_size=50)
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
