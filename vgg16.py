2eacfd0f4ce32d88a62286a188e9f89f2fcf2919
Preston Price <pwprice57@gmail.com>
2017-03-10 15:24:12 -0700

    changed epoch #

--- vgg16.py
+++ vgg16.py
@@ -127,7 +127,7 @@ model.add(MaxPooling2D((2,2), strides=(2,2)))
 # model.add(MaxPooling2D((2,2), strides=(2,2)))
 
 model.add(Flatten())
-model.add(Dense(128, activation='relu', W_constraint=maxnorm(3)))
+model.add(Dense(256, activation='relu', W_constraint=maxnorm(3)))
 model.add(Dropout(0.5))
 model.add(Dense(256, activation='relu', W_constraint=maxnorm(3)))
 model.add(Dropout(0.5))
