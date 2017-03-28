9a1b9f34fccd62be64cd0511e60c9f26fadad9dc
Preston Price <pwprice57@gmail.com>
2017-03-28 15:01:04 -0600

    update

--- predict.py
+++ predict.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-import new_method
+from new_method import Vgg16BN
 import csv
 import os.path
 from os.path import expanduser
@@ -16,12 +16,23 @@ test_path = HOME_DIR + '/test/'
 saved_model_path = HOME_DIR + '/saved-models/'
 nb_test_samples = 1000
 
+# model
+batch_size = 16
+nb_runs = 5
+nb_epoch = 10
+aug = True
+dropout = 0.05
+clip = 0.01
+use_val = False
 num_models = len(os.walk(saved_model_path).next()[2])
 
 model_name = saved_model_path + 'model' + str(num_models-1) + '.h5'
 model = load_model(model_name)
 
-predictions, f_names = model.test(test_path, nb_test_samples, aug=aug)
+vgg = Vgg16BN()
+vgg.model = model
+
+predictions, f_names = vgg.test(test_path, nb_test_samples, aug=aug)
 
 # img_names = HDF5Matrix('/home/ec2-user/img_names.hdf5', 'names', 0, 1000)
 pred_fn = saved_pred_path + 'prediction' + str(num_models) + '.csv'
